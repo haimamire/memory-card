9d5f1989d6f7ce91a3b86cb9aabb9c6d20ab9c03
haimamire <ivanmatusevich16@gmail.com>
2026-08-12 16:10:01 -0300

    feat: add scoreboard and multiple cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,30 @@
 import { useState, useEffect } from "react";
 
-export default function Card({ imgId }) {
-  const [img, setImg] = useState({ url: undefined, title: "" });
+export default function Card({ imgId, onClick }) {
+  const [img, setImg] = useState({});
 
+  // useEffect(() => {
+  //   fetch(
+  //     `https://api.giphy.com/v1/gifs/${imgId}?api_key=pnOiqSYN364PaAhhf34RpaEjgX20Ypl9`,
+  //   )
+  //     .then((response) => response.json())
+  //     .then((json) => {
+  //       const title = json.data.title;
+  //       const url = json.data.images.original.url;
+  //       setImg({ url: url, title: title });
+  //     })
+  //     .catch((error) => console.log(error));
+  // }, [imgId]);
   useEffect(() => {
-    fetch(
-      `https://api.giphy.com/v1/gifs/${imgId}?api_key=pnOiqSYN364PaAhhf34RpaEjgX20Ypl9`,
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        const title = json.data.title;
-        const url = json.data.images.original.url;
-        setImg({ url: url, title: title });
-      })
-      .catch((error) => console.log(error));
+    setTimeout(() => {
+      setImg({ title: `${imgId} loaded` });
+    }, 1000);
   }, [imgId]);
 
   return (
-    <div className="card">
+    <div className="card" onClick={onClick(imgId)}>
       <img src={img.url} alt={img.title} />
-      <h2>{img.title}</h2>
-    </div>);
+      <h2>{img.title ?? "Loading..."}</h2>
+    </div>
+  );
 }
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,9 +1,49 @@
+import { useState } from "react";
+import originalImgIds from "../data/images";
 import Card from "./Card";
 
+function Scoreboard({ score, bestScore }) {
+  return (
+    <div className="scoreboard">
+      <h2>Scoreboard</h2>
+      <p>Score: {score}</p>
+      <p>Best score: {bestScore}</p>
+    </div>
+  );
+}
+
 export default function Game() {
+  const [imgIds, setImgIds] = useState(originalImgIds);
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [guessedIds, setGuessedIds] = useState([]);
+
+  const handleClick = (currentImgId) => () => {
+    // Game over
+    if (guessedIds.includes(currentImgId)) {
+      setScore(0);
+      setGuessedIds([]);
+      alert("u lost");
+    } else {
+      const newScore = score + 1;
+      setScore(newScore);
+      setBestScore(newScore);
+      setGuessedIds([...guessedIds, currentImgId]);
+      // Winning
+      if (newScore === originalImgIds.length) {
+        alert("u won");
+      }
+    }
+  };
+
   return (
     <>
-      <Card imgId={"RfBVClg73K0uwCPVJ8"}/>
+      <Scoreboard score={score} bestScore={bestScore} />
+      <div className="cards">
+        {imgIds.map((id) => (
+          <Card key={id} imgId={id} onClick={handleClick} />
+        ))}
+      </div>
     </>
-  )
+  );
 }
